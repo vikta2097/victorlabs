53b4e4c36d3dfacba4d82894d81523777a4b7a26
Vikta <vikta2097@gmail.com>
2025-06-24 15:09:23 +0300

    Fix import paths for components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
